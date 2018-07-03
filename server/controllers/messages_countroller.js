6e83722329b313d0ea851b56c60394408709a71d
Nick Hathaway <Nicholashathaway@gmail.com>
2018-07-03 12:01:02 -0600

    finished basic lessones

--- server/controllers/messages_countroller.js
+++ server/controllers/messages_countroller.js
@@ -6,14 +6,37 @@ module.exports = {
   },
 
   create: (req, res) => {
+    const { text, time } = req.body;
+    let newMessage = { id, text, time };
+    id++;
+
+    messages.push(newMessage);
     res.status(200).send(messages);
   },
 
   update: (req, res) => {
+    let eltIndex = null;
+    messages.forEach((message, index) => {
+      if (message.id === parseInt(req.params.id)) {
+        eltIndex = index;
+      }
+    });
+    if (eltIndex !== null) {
+      messages[eltIndex] = Object.assign(messages[eltIndex], req.body);
+    }
     res.status(200).send(messages);
   },
 
   delete: (req, res) => {
+    let eltIndex = null;
+    messages.forEach((message, index) => {
+      if (message.id === parseInt(req.params.id)) {
+        eltIndex = index;
+      }
+    });
+    if (eltIndex !== null) {
+      messages.splice(eltIndex, 1);
+    }
     res.status(200).send(messages);
   },
 
--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// server-side rendering of front end code
+app.use(express.static(__dirname + '/../public/build'));
+
 const messagesController = require('./controllers/messages_countroller');
 app.get(`/api/messages`, messagesController.read);
+app.post(`/api/messages`, messagesController.create);
+app.put(`/api/messages/:id`, messagesController.update);
+app.delete(`/api/messages/:id`, messagesController.delete);
 
 const port = 3000;
 app.listen(port, () => console.log(`I'm listening... on port ${port}`));
